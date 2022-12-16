6a69dd12269e4ee97319bee215737cf8b72218b9
nick <nick@gridwebdesign.io>
2022-12-15 23:56:10 -0800

    Added gutters, moved theme switcher inside of gutter

--- public/theme-switcher.js
+++ public/theme-switcher.js
@@ -1,16 +1,6 @@
 const htmlElement = document.querySelector('html');
-const subMenuWrapper = document.querySelector('.submenu');
-const themeSwitcherButton = document.querySelector('.theme-switcher-button');
-
-const osDefaultButton = document.getElementById('os-default-theme');
 const lightButton = document.getElementById('light-theme');
 const darkButton = document.getElementById('dark-theme');
-
-// Open submenu on click
-themeSwitcherButton.addEventListener('click', () => {
-    subMenuWrapper.classList.toggle("show");
-    themeSwitcherButton.toggleAttribute('aria-expanded');
-});
 
 // Add class to body on click
 lightButton.addEventListener('click', () => {
