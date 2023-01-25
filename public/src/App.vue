2dcef93ae663fe3e5b717ee080f048b0c76d8e7c
nick <nick@gridwebdesign.io>
2023-01-24 20:52:53 -0800

    Wrangled CSS to work on homepage

--- public/src/App.vue
+++ public/src/App.vue
@@ -1,35 +1,53 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
 import NavTitle from "./components/NavTitle.vue";
-import Navigation from "./components/Navigation.vue";
+import Navigation from "./components/AppNavigation.vue";
 </script>
 
 <template>
-  <header>
-    <div class="nav-wrapper">
-      <NavTitle msg="Nutrition Planner" />
-      <Navigation />
+  <div class="cube-background-frame initial-cube">
+    <div class="cube-position-wrapper">
+      <div class="cube">
+        <div class="front"></div>
+        <div class="back"></div>
+        <div class="left"></div>
+        <div class="right"></div>
+        <div class="top"></div>
+        <div class="bottom"></div>
+      </div>
     </div>
-  </header>
-
-  <RouterView />
+  </div>
+  <div class="gutters">
+    <div class="line line-top"></div>
+    <div class="line line-right"></div>
+    <div class="line line-bottom"></div>
+    <div class="line line-left"></div>
+  </div>
+  <main>
+    <h1>Nicholas Wolch</h1>
+    <h5>Web Developer</h5>
+    <section class="content">
+      Hi there. You've stumbled onto my portfolio site. It's still in progress.
+      Check back for more soon. In the meantime feel free to reach out at
+      <a href="mailto: hey@nicholaswolch.com">hey@nicholaswolch.com</a>
+    </section>
+  </main>
+  <button id="clear-button" type="button" class="clear-button hidden">
+    Clear
+  </button>
+  <div class="theme-switcher-buttons">
+    <button id="light-theme" type="button" class="button primary has-icon">
+      <span class="button-wrap"> <i class="fa-solid fa-sun"></i> Light </span>
+    </button>
+    <button id="dark-theme" type="button" class="button primary has-icon">
+      <span class="button-wrap"> <i class="fa-solid fa-moon"></i> Dark </span>
+    </button>
+  </div>
 </template>
 
+<style scoped lang="css" src="./assets/styles.css"></style>
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-  padding-top: 5vh;
-}
-header {
-  display: flex;
-  place-items: flex-start;
-}
-
-header .nav-wrapper {
-  display: flex;
-  align-items: flex-start;
-  flex-wrap: wrap;
-  padding: 2rem;
+main {
+  width: 100vw;
 }
 </style>
--- public/src/router/index.ts
+++ public/src/router/index.ts
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import FoodSearchView from "../views/FoodSearchView.vue";
 import AboutView from "../views/AboutView.vue";
+import NutritionApp from "../views/NutritionApp.vue";
+import NutritionAppHome from "../views/NutritionAppHome.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,12 +14,22 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: "/food-search",
+      path: "/nutrition-app",
+      name: "nutritionApp",
+      component: NutritionApp,
+    },
+    {
+      path: "/nutrition-app-home",
+      name: "nutritionAppHome",
+      component: NutritionAppHome,
+    },
+    {
+      path: "/nutrition-app/food-search",
       name: "foodSearch",
       component: FoodSearchView,
     },
     {
-      path: "/about",
+      path: "/nutrition-app/about",
       name: "about",
       component: AboutView,
     },
