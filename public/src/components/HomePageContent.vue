f6735be2c14ac53bb2749bfd7cb52658cc73201e
nick <nick@gridwebdesign.io>
2023-01-26 21:30:46 -0800

    Started on home navigation

--- public/src/components/HomePageContent.vue
+++ public/src/components/HomePageContent.vue
@@ -1,6 +1,7 @@
 <template>
   <h1>Nicholas Wolch</h1>
   <h5>Web Developer</h5>
+  <HomepageNavigation />
   <section class="content">
     Hi there. You've stumbled onto my portfolio site. It's still in progress.
     Check back for more soon. In the meantime feel free to reach out at
@@ -9,9 +10,10 @@
 </template>
 
 <script lang="ts">
+import HomepageNavigation from "./HomepageNavigation.vue";
 export default {
   name: "HomePageContent",
-  components: {},
+  components: { HomepageNavigation },
   props: {},
   setup() {
     return {};
---
+++ public/src/components/HomepageNavigation.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="nav-container">
+    <button @click="isOpen = !isOpen" class="hamburger">
+      <span class="hamburger-line"></span>
+      <span class="hamburger-line"></span>
+      <span class="hamburger-line"></span>
+    </button>
+    <transition name="slide-down">
+      <nav>
+        <ul v-if="isOpen" class="nav-items">
+          <li v-for="item in items" :key="item.id">
+            <router-link :to="item.path">{{ item.name }}</router-link>
+          </li>
+        </ul>
+      </nav>
+    </transition>
+  </div>
+</template>
+<script lang="ts">
+export default {
+  name: "HomepageNavigation",
+  props: {},
+  setup() {
+    return {};
+  },
+  data() {
+    return {
+      isOpen: false,
+      items: [
+        { id: 1, name: "Home", path: "/" },
+        { id: 2, name: "About", path: "/about" },
+        { id: 3, name: "Contact", path: "/contact" },
+      ],
+    };
+  },
+};
+</script>
+<style scoped>
+.nav-container {
+  position: relative;
+}
+
+.nav-items {
+  position: absolute;
+  top: 100%;
+  left: 0;
+  width: 100%;
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  background-color: #fff;
+}
+
+.hamburger {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 3rem;
+  height: 3rem;
+  padding: 1rem 0 0 0;
+  background: none;
+  border: none;
+}
+
+.hamburger-line {
+  width: 100%;
+  height: 3px;
+  background-color: var(--secondary-color);
+}
+
+.slide-down-enter-active,
+.slide-down-leave-active {
+  transition: all 0.5s ease;
+}
+.slide-down-enter,
+.slide-down-leave-to {
+  transform: translateY(-100%);
+}
+</style>
