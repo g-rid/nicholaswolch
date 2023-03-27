37d581a8dc02e4ffd5576bfd8978736f003e9546
nick <nick@gridwebdesign.io>
2023-03-27 00:49:34 -0700

    Got both objects to merge, still working on displaying them appropriately

--- src/components/FoodNutrients.vue
+++ src/components/FoodNutrients.vue
@@ -7,14 +7,24 @@
       <thead>
         <tr>
           <th>Nutrient:</th>
+          <th>Type:</th>
           <th>Units:</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(nutrient, key) in selectedItem.foodNutrients"
+        <tr v-for="(nutrient, key) in combinedNutrientData"
       :key="key">
-          <td>{{ nutrient.nutrientName }}</td>
-          <td>{{ nutrient.value }} {{ nutrient.unitName }}</td>
+          <td>{{ nutrient.name }} <button @click="showInfoModal = true"><font-awesome-icon :icon="['fas', 'circle-info']" /></button></td>
+          <td>{{ nutrient.type }}</td>
+          <td>{{ nutrient.value }} {{ nutrient.unit }}</td>
+          <div class="nutrient-info-modal" v-if="showInfoModal">
+            <div class="nutrient-info-modal-content">
+              <h3>{{ nutrient.name }}</h3>
+              <button @click="showInfoModal = false">X</button>
+              {{ nutrient.whatItDoes }}
+              {{ nutrient.whereItsFound }}
+            </div>
+          </div>
         </tr>
       </tbody>
     </table>
@@ -23,6 +33,9 @@
 
 <script lang="ts">
 import type { PropType } from "vue";
+import { ref } from "vue";
+import defaultNutrients from "./data/defaultNutrients.json";
+import nutrientMap from "./data/nutrientMap.json";
 
 export default {
   name: "FoodNutrients",
@@ -37,12 +50,35 @@ export default {
       required: false,
     },
   },
+  setup(props) {
+    const selectedItem = props.selectedItem;
+      
+    function combineNutrientData(selectedInfo, defaultInfo) {
+    if (!Array.isArray(selectedInfo)) {
+      selectedInfo = [selectedInfo];
+    }
+    const combinedData = selectedInfo.map(nutrient => {
+    const nutrientName = nutrientMap[nutrient.nutrientName];
+    const nutrientInfo = defaultInfo[nutrientName];
+    return {
+      name: nutrient.nutrientName,
+      value: nutrient.value,
+      unit: nutrient.unitName,
+      whereItsFound: nutrientInfo?.whereItsFound,
+      whatItDoes: nutrientInfo?.whatItDoes,
+      type: nutrientInfo?.type,
+    };
+  });
+    return combinedData;
+  }
+  const combinedNutrientData = combineNutrientData(selectedItem.foodNutrients, defaultNutrients);
+  console.log("combinedNutrientData", combinedNutrientData);
 
-  setup() {
-
+  const showInfoModal = ref(false);
 
     return {
-    
+      combinedNutrientData,
+      showInfoModal,
     };
   },
   methods: {
@@ -53,4 +89,21 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.nutrient-info-modal {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.nutrient-info-modal-content {
+  background-color: var(--secondary-color);
+  color: var(--primary-color);
+  padding: 20px;
+}
+</style>
