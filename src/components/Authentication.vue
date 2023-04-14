ce5fe31b924bca9c0e07ee90bb61fa9345323765
nick <nick@gridwebdesign.io>
2023-04-14 13:35:30 -0700

    Authentication component

---
+++ src/components/Authentication.vue
@@ -0,0 +1,30 @@
+<template>
+    <div v-if="!isAuthenticated">
+      <h1>Please sign in</h1>
+      <button @click="signIn">Sign in with email and password</button>
+    </div>
+    <div v-else>
+      <h1>Welcome, {{ userDisplayName }}</h1>
+      <button @click="signOut">Sign out</button>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent, computed } from 'vue';
+  import { user, isAuthenticated, signIn, signOut } from '@/auth';
+  
+  export default defineComponent({
+    setup() {
+      const userDisplayName = computed(() => {
+        return user.value?.displayName || 'User';
+      });
+  
+      return {
+        userDisplayName,
+        isAuthenticated,
+        signIn,
+        signOut
+      };
+    }
+  });
+  </script>
