c621aa2c0982174a7bb168abd6ed46ffa300dffe
nick <nick@gridwebdesign.io>
2023-04-14 13:35:41 -0700

    Authentication component

--- src/views/FoodSearchView.vue
+++ src/views/FoodSearchView.vue
@@ -1,15 +1,18 @@
 <script lang="ts">
 import FoodSearch from "../components/FoodSearch.vue";
+import Authentication from "../components/Authentication.vue";
 export default {
   name: "FoodSearchView",
   components: {
     FoodSearch,
+    Authentication,
   },
 };
 </script>
 
 <template>
-    <FoodSearch />
+    <!-- <FoodSearch /> -->
+    <Authentication />
 </template>
 
 <style>
