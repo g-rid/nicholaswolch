c442e780d99989413d3e03294f10602cc432a0eb
nick <nick@gridwebdesign.io>
2023-04-15 14:01:17 -0700

    Continued to get Auth working

--- src/components/Authentication.vue
+++ src/components/Authentication.vue
@@ -1,30 +1,41 @@
 <template>
-    <div v-if="!isAuthenticated">
-      <h1>Please sign in</h1>
-      <button @click="signIn">Sign in with email and password</button>
-    </div>
-    <div v-else>
-      <h1>Welcome, {{ userDisplayName }}</h1>
-      <button @click="signOut">Sign out</button>
-    </div>
-  </template>
-  
-  <script lang="ts">
-  import { defineComponent, computed } from 'vue';
-  import { user, isAuthenticated, signIn, signOut } from '@/auth';
-  
-  export default defineComponent({
-    setup() {
-      const userDisplayName = computed(() => {
-        return user.value?.displayName || 'User';
-      });
-  
-      return {
-        userDisplayName,
-        isAuthenticated,
-        signIn,
-        signOut
-      };
-    }
-  });
-  </script>
+  <div v-if="!isAuthenticated">
+    <h1>Please sign in</h1>
+    <input type="email" v-model="email" placeholder="Email" />
+    <input type="password" v-model="password" placeholder="Password" />
+    <button @click="handleSignIn">Sign in with email and password</button>
+  </div>
+  <div v-else>
+    <h1>Welcome, {{ userDisplayName }}</h1>
+    <button @click="signOut">Sign out</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed, ref } from 'vue';
+import { user, isAuthenticated, signIn, signOut } from '../auth';
+
+export default defineComponent({
+  setup() {
+    const email = ref('');
+    const password = ref('');
+    const userDisplayName = computed(() => {
+      return user.value?.displayName || 'User';
+    });
+    const isUserAuthenticated = computed(() => isAuthenticated.value);
+
+    const handleSignIn = async () => {
+      await signIn(email.value, password.value);
+    };
+
+    return {
+      email,
+      password,
+      userDisplayName,
+      isAuthenticated: isUserAuthenticated,
+      handleSignIn,
+      signOut,
+    };
+  },
+});
+</script>
